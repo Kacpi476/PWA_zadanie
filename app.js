bdc086de2b6807bc9d0cffbad3444a04415aba08
PiotrSzarekinf <dirtyannus@gmail.com>
2026-03-23 13:52:45 +0100

    Pobieranie lokalizacji

--- app.js
+++ app.js
@@ -5,10 +5,22 @@ const captureButton = document.querySelector('#capture');
 navigator.mediaDevices.getUserMedia({ video: true })
     .then((stream) => { video.srcObject = stream; });
 
+let currentCoords = { lat: 0, lng: 0 };
+
     captureButton.addEventListener('click', () => {
         
         const context = canvas.getContext('2d');
         context.drawImage(video, 0, 0, canvas.width, canvas.height);
+ if ("geolocation" in navigator) {
+        navigator.geolocation.getCurrentPosition((position) => {
+            currentCoords.lat = position.coords.latitude;
+            currentCoords.lng = position.coords.longitude;
+            
+            alert(`Znalezisko zarejestrowane! Współrzędne: ${currentCoords.lat}, ${currentCoords.lng}`);
+            // Tutaj możesz dodać kod Leaflet.js, aby postawić pinezkę na mapie 
+        });
+    }
+});
 
-    });
+    
     //asdadsasd
