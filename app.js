b0a446f334ffa776f79cb888a54cf55bf4df1e3f
s-A121-30 <s-A121-30@CAMPUS.UEK.KRAKOW.PL>
2026-03-23 13:43:44 +0100

    logika zdjęć

---
+++ app.js
@@ -0,0 +1,13 @@
+const video = document.querySelector('#player');
+const canvas = document.querySelector('#canvas');
+const captureButton = document.querySelector('#capture');
+
+navigator.mediaDevices.getUserMedia({ video: true })
+    .then((stream) => { video.srcObject = stream; });
+
+    captureButton.addEventListener('click', () => {
+        
+        const context = canvas.getContext('2d');
+        context.drawImage(video, 0, 0, canvas.width, canvas.height);
+
+    });
